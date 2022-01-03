c095b3ef75176d1bf7c0b3642a97a3fe5b673f84
tadejrebernjak <tadej.rebernjak2@gmail.com>
2022-01-03 14:19:39 +0100

    comments frontend

--- news-combinator/src/views/Article.vue
+++ news-combinator/src/views/Article.vue
@@ -38,43 +38,86 @@
     </div>
   </div>
   <h1 class="mt-10 text-2xl font-bold">Komentarji ({{ counters.comments }})</h1>
-  <div class="new-comment-container">
-    <textarea
-      class="w-full p-3 my-2 border-2 border-green-500 bg-white rounded-lg"
-      rows="4"
-      v-model="newComment"
-    ></textarea>
-    <button class="new-comment-btn" v-on:click="addComment">
-      <i class="fas fa-paper-plane"></i>
-      <span> Dodaj</span>
-    </button>
-  </div>
-  <div class="comments-container">
-    <div
-      class="comment"
-      v-for="(comment, index) in article.comments"
-      :item="comment"
-      :index="index"
-      :key="comment.userID"
-    >
-      <div class="comment-head">
-        <p class="text-lg font-bold text-green-500">{{ comment.username }}</p>
-        <p class="text-sm font-bold text-gray-500">
-          {{
-            comment.createdAt.getDate() +
-            "." +
-            (comment.createdAt.getMonth() + 1) +
-            "." +
-            comment.createdAt.getFullYear() +
-            " ob " +
-            comment.createdAt.getHours() +
-            ":" +
-            comment.createdAt.getMinutes()
-          }}
-        </p>
+  <div class="comments-feed">
+    <div class="new-comment-container">
+      <div class="flex">
+        <img
+          src="../assets/default-pfp.jpg"
+          alt="Pfp"
+          class="inline w-14 h-14 rounded-full mr-3 shadow-md"
+        />
+        <div class="w-full">
+          <h2 class="text-left font-bold text-white text-xl mb-1">Tadej</h2>
+          <textarea
+            class="new-comment-textarea"
+            rows="4"
+            placeholder="Napišite komentar o članku!"
+            v-model="newComment"
+          ></textarea>
+        </div>
       </div>
-      <div class="comment-body">
-        <p class="comment-text">{{ comment.body }}</p>
+      <button class="new-comment-btn" v-on:click="addComment">
+        <i class="fas fa-paper-plane"></i>
+        <span> Dodaj</span>
+      </button>
+    </div>
+    <div class="comments-container">
+      <div
+        class="comment"
+        v-for="(comment, index) in article.comments"
+        :item="comment"
+        :index="index"
+        :key="comment.userID"
+      >
+        <div class="comment-left">
+          <img src="../assets/default-pfp.jpg" alt="pfp" />
+        </div>
+        <div class="comment-right">
+          <div class="comment-head">
+            <p class="text-lg font-bold text-green-500">
+              {{ comment.username }}
+            </p>
+            <p class="text-sm font-semibold text-gray-500">
+              {{
+                comment.createdAt.getDate() +
+                "." +
+                (comment.createdAt.getMonth() + 1) +
+                "." +
+                comment.createdAt.getFullYear() +
+                " ob " +
+                comment.createdAt.getHours() +
+                ":" +
+                comment.createdAt.getMinutes()
+              }}
+            </p>
+          </div>
+          <div class="comment-body">
+            <p class="comment-text">{{ comment.body }}</p>
+          </div>
+          <div class="comment-foot">
+            <button class="reply-button">
+              <i class="fas fa-reply"></i> Odgovori
+            </button>
+            <div
+              class="grid grid-cols-2 space-x-3 text-lg mr-2 font-bold text-gray-600"
+            >
+              <div
+                class="text-center hover:text-gray-500 cursor-pointer transition-all duration-300"
+                v-on:click="likeComment(index)"
+              >
+                <i class="fas fa-thumbs-up text-2xl"></i>
+                <p>{{ comment.likes.length }}</p>
+              </div>
+              <div
+                class="text-center hover:text-gray-500 cursor-pointer transition-all duration-300"
+                v-on:click="dislikeComment(index)"
+              >
+                <i class="fas fa-thumbs-down text-2xl"></i>
+                <p>{{ comment.dislikes.length }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -161,12 +204,19 @@ export default {
         this.article.comments.push(comment);
         this.counters.comments = this.article.comments.length;
         console.log(this.counters.comments);
+        this.newComment = "";
       }
     },
     addZero(time) {
       if (time < 10) {
         time = "0" + time;
       }
+    },
+    likeComment(i) {
+      this.article.comments[i].likes.push(1);
+    },
+    dislikeComment(i) {
+      this.article.comments[i].dislikes.push(1);
     },
   },
   beforeMount() {
@@ -231,27 +281,55 @@ export default {
   @apply text-red-500 hover:text-red-600;
 }
 
+.comments-feed {
+  @apply my-3 rounded-lg border-2 border-green-500 shadow-2xl overflow-hidden;
+}
+
 .new-comment-container {
-  @apply p-3 my-3 bg-gradient-to-r from-green-400 to-green-500 rounded-lg text-right;
+  @apply p-3 bg-gradient-to-t from-green-400 to-green-500 text-right;
+}
+
+.new-comment-textarea {
+  @apply w-full p-3 mb-2 border-2 shadow-md border-green-500 bg-white rounded-lg focus:outline-none focus:border-green-600 focus:bg-gray-50;
 }
 
 .new-comment-btn {
-  @apply p-2 pr-3 w-28 border-2 border-green-500 bg-gray-50 hover:bg-gray-100 rounded-lg text-lg;
+  @apply p-2 pr-3 font-bold w-28 border-2 shadow-md border-green-500 bg-gray-50 hover:bg-gray-100 rounded-lg text-lg;
 }
 
 .comments-container {
-  @apply my-10 p-7 bg-gradient-to-b from-emerald-600 to-emerald-500 rounded-lg;
+  @apply px-3 bg-gradient-to-b bg-gray-50;
 }
 
 .comment {
-  @apply p-4 py-2 my-3 border border-gray-400 bg-gray-100 rounded-lg;
+  @apply p-3 my-3 shadow-md border-2 border-gray-200 bg-white rounded-lg flex;
+}
+
+.comment-left {
+  @apply inline mr-3;
+}
+
+.comment-left img {
+  @apply w-14 rounded-full shadow-md;
+}
+
+.comment-right {
+  @apply w-full;
 }
 
 .comment-head {
-  @apply mb-2 py-2 border-b border-gray-400;
+  @apply mb-2 pb-2 border-b border-gray-300;
 }
 
 .comment-text {
   white-space: pre-wrap;
+}
+
+.comment-foot {
+  @apply my-2 flex justify-between;
+}
+
+.reply-button {
+  @apply p-1 text-green-400 uppercase font-bold hover:text-green-500 transition-all;
 }
 </style>
